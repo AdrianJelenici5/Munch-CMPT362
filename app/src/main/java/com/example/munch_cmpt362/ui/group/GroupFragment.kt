85e13c414264ed9f69e234a64714be2df4ae9787
thedarianwong <thedarianwong@gmail.com>
2024-12-02 16:17:43 -0800

    Added permissions for camera and storage to choose profile pictures and upload to storage

--- app/src/main/java/com/example/munch_cmpt362/ui/group/GroupFragment.kt
+++ app/src/main/java/com/example/munch_cmpt362/ui/group/GroupFragment.kt
@@ -87,132 +87,145 @@ class GroupFragment: Fragment() {
 //        }
         // TRYING OUT THE FIREBASE
         val authViewModel = ViewModelProvider(requireActivity()).get(AuthViewModel::class.java)
-        val userID = authViewModel.returnID()!!.uid
 
-        groupFbList = ArrayList()
-        myGroupFbListAdapter = GroupFbListAdapter(requireActivity(), groupFbList)
-        myGroupListView.adapter = myGroupFbListAdapter
+        val currentUser = authViewModel.returnID()
+        if (currentUser != null) {
+            val userID = currentUser.uid
 
-        println("GABRIEL THIS IS ID: $userID")
-        val database = Firebase.firestore
-        // Collection = entry, query = field
-        CoroutineScope(IO).launch {
-            database.collection("users").whereEqualTo("userId", userID).get()
-                .addOnSuccessListener { documents ->
-                    for (document in documents) {
-                        Log.d("TAG", "GABRIEL ${document.id} => ${document.data}")
+
+            groupFbList = ArrayList()
+            myGroupFbListAdapter = GroupFbListAdapter(requireActivity(), groupFbList)
+            myGroupListView.adapter = myGroupFbListAdapter
+
+            println("GABRIEL THIS IS ID: $userID")
+            val database = Firebase.firestore
+            // Collection = entry, query = field
+            CoroutineScope(IO).launch {
+                database.collection("users").whereEqualTo("userId", userID).get()
+                    .addOnSuccessListener { documents ->
+                        for (document in documents) {
+                            Log.d("TAG", "GABRIEL ${document.id} => ${document.data}")
+                        }
                     }
-                }
-                .addOnFailureListener { exception ->
-                    Log.w("TAG", "Error getting documents: ", exception)
-                }
-        }
-
-        // Adding groups to grouplistview
-        CoroutineScope(IO).launch {
-            database.collection("group").whereArrayContains("listOfUserIds", userID).get()
-                .addOnSuccessListener { documents ->
-                    for (document in documents) {
-                        var groupFb = GroupFb()
-                        Log.d("TAG", "GABRIEL GROUP ${document.id} => ${document.data}")
-                        groupFb.groupId = document.id
-                        groupFb.groupName = document.data["groupName"].toString()
-                        groupFb.listOfUserIds = document.data["listOfUserIds"] as MutableList<String>
-                        groupFb.listOfRestaurants = document.data["listOfRestaurants"] as MutableList<String>
-                        groupFbList.add(groupFb)
+                    .addOnFailureListener { exception ->
+                        Log.w("TAG", "Error getting documents: ", exception)
                     }
-                    myGroupFbListAdapter.replace(groupFbList)
-                    myGroupFbListAdapter.notifyDataSetChanged()
-                }
-                .addOnFailureListener { exception ->
-                    Log.w("TAG", "Error getting documents: ", exception)
-                }
-        }
-        myGroupFbViewModel = ViewModelProvider(requireActivity()).get(GroupFbViewModel::class.java)
-        // Initialize counter
+            }
+
+            // Adding groups to grouplistview
+            CoroutineScope(IO).launch {
+                database.collection("group").whereArrayContains("listOfUserIds", userID).get()
+                    .addOnSuccessListener { documents ->
+                        for (document in documents) {
+                            var groupFb = GroupFb()
+                            Log.d("TAG", "GABRIEL GROUP ${document.id} => ${document.data}")
+                            groupFb.groupId = document.id
+                            groupFb.groupName = document.data["groupName"].toString()
+                            groupFb.listOfUserIds =
+                                document.data["listOfUserIds"] as MutableList<String>
+                            groupFb.listOfRestaurants =
+                                document.data["listOfRestaurants"] as MutableList<String>
+                            groupFbList.add(groupFb)
+                        }
+                        myGroupFbListAdapter.replace(groupFbList)
+                        myGroupFbListAdapter.notifyDataSetChanged()
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.w("TAG", "Error getting documents: ", exception)
+                    }
+            }
+            myGroupFbViewModel =
+                ViewModelProvider(requireActivity()).get(GroupFbViewModel::class.java)
+            // Initialize counter
 //        if(groupViewModel.getCounter() == null){
 //            var counter = Counter()
 //            groupViewModel.insertCounter(counter)
 //        }
 
-        addGroupButton.setOnClickListener(){
-            //val addGroupDialog = AddGroupDialog()
-            val addGroupDialog = AddGroupFbDialog()
-            addGroupDialog.show(parentFragmentManager, "add group")
+            addGroupButton.setOnClickListener() {
+                //val addGroupDialog = AddGroupDialog()
+                val addGroupDialog = AddGroupFbDialog()
+                addGroupDialog.show(parentFragmentManager, "add group")
 
-            parentFragmentManager.executePendingTransactions()
-            addGroupDialog.dialog?.setOnDismissListener {
-                if(myGroupFbViewModel.groupAddedName.value != null){
-                    // Use groupAddedName and update database and list
-                    val authViewModel = ViewModelProvider(requireActivity()).get(AuthViewModel::class.java)
-                    val userID = authViewModel.returnID()!!.uid
+                parentFragmentManager.executePendingTransactions()
+                addGroupDialog.dialog?.setOnDismissListener {
+                    if (myGroupFbViewModel.groupAddedName.value != null) {
+                        // Use groupAddedName and update database and list
+                        val authViewModel =
+                            ViewModelProvider(requireActivity()).get(AuthViewModel::class.java)
+                        val userID = authViewModel.returnID()!!.uid
 
-                    CoroutineScope(IO).launch{
-                        // Get users list of restaurants and add it to group list
-                        val userPrefRef = Firebase.firestore.collection("user-preference").document(userID).get()
-                            .addOnSuccessListener { document ->
-                                val listRestaurant: MutableList<String> = ArrayList()
-                                val rest1 = document.data!!["restaurant_1"].toString()
-                                val rest2 = document.data!!["restaurant_2"].toString()
-                                val rest3 = document.data!!["restaurant_3"].toString()
-                                if(rest1 != ""){
-                                    listRestaurant.add(rest1)
-                                }
-                                if(rest2 != ""){
-                                    listRestaurant.add(rest2)
-                                }
-                                if(rest3 != ""){
-                                    listRestaurant.add(rest3)
-                                }
+                        CoroutineScope(IO).launch {
+                            // Get users list of restaurants and add it to group list
+                            val userPrefRef =
+                                Firebase.firestore.collection("user-preference").document(userID)
+                                    .get()
+                                    .addOnSuccessListener { document ->
+                                        val listRestaurant: MutableList<String> = ArrayList()
+                                        val rest1 = document.data!!["restaurant_1"].toString()
+                                        val rest2 = document.data!!["restaurant_2"].toString()
+                                        val rest3 = document.data!!["restaurant_3"].toString()
+                                        if (rest1 != "") {
+                                            listRestaurant.add(rest1)
+                                        }
+                                        if (rest2 != "") {
+                                            listRestaurant.add(rest2)
+                                        }
+                                        if (rest3 != "") {
+                                            listRestaurant.add(rest3)
+                                        }
 
-                                // Add list to group database
-                                val ref = Firebase.firestore.collection("group").document()
-                                val list = ArrayList<String>()
-                                list.add(userID)
-                                val groupSet = hashMapOf(
-                                    "groupId" to ref.id,
-                                    "groupName" to myGroupFbViewModel.groupAddedName.value,
-                                    "listOfRestaurants" to listRestaurant,
-                                    "listOfUserIds" to list,
-                                )
-                                ref.set(groupSet)
-                                val groupFb = GroupFb()
-                                groupFb.groupId = ref.id
-                                groupFb.groupName = myGroupFbViewModel.groupAddedName.value!!
-                                groupFb.listOfRestaurants = listRestaurant
-                                groupFb.listOfUserIds = list
-                                //myGroupFbViewModel.groupFb.postValue(groupFb)
-                                groupFbList.add(groupFb)
-                                myGroupFbListAdapter.replace(groupFbList)
-                                myGroupFbListAdapter.notifyDataSetChanged()
-                                myGroupFbViewModel.groupAddedName.value = null
-                            }
-                            .addOnFailureListener { exception ->
-                                Log.w("TAG", "Error getting documents: ", exception)
-                            }
-                    }
+                                        // Add list to group database
+                                        val ref = Firebase.firestore.collection("group").document()
+                                        val list = ArrayList<String>()
+                                        list.add(userID)
+                                        val groupSet = hashMapOf(
+                                            "groupId" to ref.id,
+                                            "groupName" to myGroupFbViewModel.groupAddedName.value,
+                                            "listOfRestaurants" to listRestaurant,
+                                            "listOfUserIds" to list,
+                                        )
+                                        ref.set(groupSet)
+                                        val groupFb = GroupFb()
+                                        groupFb.groupId = ref.id
+                                        groupFb.groupName =
+                                            myGroupFbViewModel.groupAddedName.value!!
+                                        groupFb.listOfRestaurants = listRestaurant
+                                        groupFb.listOfUserIds = list
+                                        //myGroupFbViewModel.groupFb.postValue(groupFb)
+                                        groupFbList.add(groupFb)
+                                        myGroupFbListAdapter.replace(groupFbList)
+                                        myGroupFbListAdapter.notifyDataSetChanged()
+                                        myGroupFbViewModel.groupAddedName.value = null
+                                    }
+                                    .addOnFailureListener { exception ->
+                                        Log.w("TAG", "Error getting documents: ", exception)
+                                    }
+                        }
 //                    groupFbList.add(myGroupFbViewModel.groupFb.value!!)
 //                    myGroupFbListAdapter.replace(groupFbList)
 //                    myGroupFbListAdapter.notifyDataSetChanged()
 //                    myGroupFbViewModel.groupAddedName.value = null
+                    }
+                    (parentFragmentManager.findFragmentByTag("add group") as DialogFragment).dismiss()
                 }
-                (parentFragmentManager.findFragmentByTag("add group") as DialogFragment).dismiss()
             }
-        }
 //
-        myGroupListView.setOnItemClickListener(){ parent, view, position, id ->
-            Toast.makeText(requireActivity(), "Stub to display members", Toast.LENGTH_SHORT).show()
-            // Get group object
-            var groupFb: GroupFb = myGroupFbListAdapter.getItem(position)
-            // Start the GROUP MEMBERS fragment, initialize group object in it
+            myGroupListView.setOnItemClickListener() { parent, view, position, id ->
+                Toast.makeText(requireActivity(), "Stub to display members", Toast.LENGTH_SHORT)
+                    .show()
+                // Get group object
+                var groupFb: GroupFb = myGroupFbListAdapter.getItem(position)
+                // Start the GROUP MEMBERS fragment, initialize group object in it
 //            val groupMemberFragment = GroupMembersFragment()
 //            groupMemberFragment.group = group
 //            requireActivity().supportFragmentManager.beginTransaction()
 //                .replace(android.R.id.content, groupMemberFragment).addToBackStack("group tag").commit()
 
-            // Add clicked group to viewmodel
-            myGroupFbViewModel.clickedGroup.value = groupFb
-            findNavController().navigate(R.id.action_mainFragment_to_groupMembersFragment)
+                // Add clicked group to viewmodel
+                myGroupFbViewModel.clickedGroup.value = groupFb
+                findNavController().navigate(R.id.action_mainFragment_to_groupMembersFragment)
+            }
         }
 
 
