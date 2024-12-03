ce6d14a27f705cd10003182d8e8fb523b2c34414
thedarianwong <thedarianwong@gmail.com>
2024-12-02 18:28:39 -0800

    Minor change on Discover ViewModel to include the set distance fetched for radius param

--- app/src/main/java/com/example/munch_cmpt362/ui/discover/DiscoverViewModel.kt
+++ app/src/main/java/com/example/munch_cmpt362/ui/discover/DiscoverViewModel.kt
@@ -14,13 +14,13 @@ class DiscoverViewModel : ViewModel() {
 
     private var dataFetched = false
 
-    fun fetchRestaurants(latitude: Double, longitude: Double) {
+    fun fetchRestaurants(latitude: Double, longitude: Double, radius: Int = 5000) {
 
 //        Log.d("XD:", "XD: ReviewViewModel | Lat: $latitude, Long: $longitude")
         if (dataFetched) return
 //        Log.d("XD:", "XD: passed if case")
 
-        ApiHelper.callYelpNearbyRestaurantsApi(latitude, longitude) { response ->
+        ApiHelper.callYelpNearbyRestaurantsApi(latitude, longitude, radius) { response ->
             Log.d("XD:", "XD: Received response: $response")
             response?.businesses?.let {
                 Log.d("XD:", "XD: Updating restaurants LiveData")
